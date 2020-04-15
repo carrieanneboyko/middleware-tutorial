ea8ca1821fa9a4b61982b25a567d64bdb4e6aaca
Brian Boyko <brian.boyko@publicissapient.com>
2020-04-15 17:52:41 +0100

    env variables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import executeServer from './server';
+import dotenv from "dotenv";
+
+if(process.env.NODE_ENV === undefined){
+  dotenv.config({ path: path.join(__dirname, '../../.env') });
+  console.log("process.env.NODE_ENV after:", process.env.NODE_ENV);
+}
 
 executeServer(); 
