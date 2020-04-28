501f32bac08c283db20161a86294d17c83570066
brianboyko <brian.boyko@gmail.com>
2020-04-28 15:33:05 +0100

    Delete unnecessary

--- src/server/index.js
+++ src/server/index.js
@@ -1,77 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
-import simpleLogger from "./middleware/simpleLogger";
-
-import fs from "fs";
-import path from "path";
-import addToDatabase from "./middleware/addToDatabase";
-import getFromDatabaseByNumber from "./middleware/getFromDatabaseByNumber";
+import morgan from "morgan";
+import cors from "cors";
 
 const launchServer = (port) => {
   const app = express();
 
-  app.use(simpleLogger);
+  app.use(morgan);
+  app.use(cors());
   app.use(bodyParser.json());
 
-  // handler for the /user/:id path, which prints the user ID to the console
-
-  app.get(
-    "/user/:id",
-    (req, res, next) => {
-      console.log("ID:", req.params.id);
-      if (["bozo", "pennywise", "krusty", "ronald"].includes(req.params.id)) {
-        console.warn("CLOWN DETECTED");
-        next(); // go to the next function in this function chain;
-      } else {
-        next("route"); // go to the next matching route, skip the other functions;
-      }
-    },
-    (req, res, next) => {
-      res.send(`Hey! I thought I told you clowns to get out of here!`);
-    }
-  );
-
-  // handler for the /user/:id path, which prints the user ID to the browser
-  app.get("/user/:id", function (req, res, next) {
-    res.send(`User is ${req.params.id}`);
-  });
-
-  app.get("/hello-world", (req, res) => {
+  app.get("/hello-world", (_req, res) => {
     res.send("Hello World!");
   });
 
-  app.post("/hello-world", (req, res) => {
+  app.post("/hello-world", (_req, res) => {
     res.send("Hello World From Post!");
-  });
-
-  app.post("/save", addToDatabase);
-
-  app.get(
-    "/byNumber/:number",
-    (req, res, next) => {
-      console.log({ number: req.params.number });
-      next();
-    },
-    getFromDatabaseByNumber
-  );
-
-  app.get("/file", function (req, res, next) {
-    console.log(path.resolve(__dirname, "../../filemaybe.txt"));
-    fs.readFile(path.resolve(__dirname, "../../filemaybe.txt"), function (
-      err,
-      data
-    ) {
-      if (err) {
-        next(err); // Pass errors to Express.
-      } else {
-        res.send(data);
-      }
-    });
   });
 
   // error handlers
 
-  app.use(function (err, req, res, next) {
+  app.use(function (err, _req, res, _next) {
     console.error(err.stack);
     res.status(500).send("Something broke!");
   });
--- src/server/middleware/simpleLogger.js
+++
@@ -1,8 +0,0 @@
-// new file: ./src/server/middleware/simpleLogger.js
-
-const simpleLogger = (req, _res, next) => {
-  console.log(`Request Type: ${req.method} received at ${Date.now()}`);
-  next(); 
-}
-
-export default simpleLogger; 
